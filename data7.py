52778e0b5e4cd596f818ef74c0ae287a6d31c9b1
murakami <96195951+hiro1103@users.noreply.github.com>
2022-04-15 01:37:36 +0900

    Add files via upload

--- data7.py
+++ data7.py
@@ -1,6 +1,8 @@
+
 from matplotlib.pyplot import clf
 from sklearn.base import BaseEstimator
 from sklearn.base import ClassifierMixin
+from sklearn.ensemble import BaggingClassifier
 from sklearn.preprocessing import LabelEncoder
 from sklearn.base import clone
 from sklearn.pipeline import _name_estimators
@@ -12,12 +14,12 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.pipeline import Pipeline
-from sklearn.metrics import roc_curve
+from sklearn.metrics import accuracy_score, roc_curve
 from sklearn.metrics import auc
 from itertools import product
 import numpy as np
 import matplotlib.pyplot as plt
-from zmq import get_library_dirs
+import pandas as pd
 
 
 class MajorityVoteClassifier(BaseEstimator,
@@ -226,3 +228,80 @@ for r, _ in enumerate(grid.cv_results_['mean_test_score']):
 
 print('Best parameters: %s' % grid.best_params_)
 print('Accuracy: %.2f' % grid.best_score_)
+
+df_wine = pd.read_csv(
+    'https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data',
+    header=None
+)
+
+df_wine.columns = ['Class label', 'Alcohol', 'Malic acid', 'Ash',
+                   'Alcalinity of ash', 'magnesium', 'Total phenols', 'Flavanoids',
+                   'Nonflavanoid phenols', 'Proanthocyanins', 'Color intensity',
+                   'Hue', 'OD280/OD315 of diluted wines', 'Proline']
+
+# クラス1を削除
+df_wine = df_wine[df_wine['Class label'] != 1]
+y = df_wine['Class label'].values
+X = df_wine[['Alcohol', 'OD280/OD315 of diluted wines']].values
+
+le = LabelEncoder()
+y = le.fit_transform(y)
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                    test_size=0.2,
+                                                    random_state=1,
+                                                    stratify=y)
+tree = DecisionTreeClassifier(criterion='entropy',
+                              max_depth=None,
+                              random_state=1)
+
+bag = BaggingClassifier(base_estimator=tree,
+                        n_estimators=500,
+                        max_samples=1.0,
+                        max_features=1.0,
+                        bootstrap=True,
+                        bootstrap_features=False,
+                        n_jobs=1,
+                        random_state=1
+                        )
+
+tree = tree.fit(X_train, y_train)
+y_train_pred = tree.predict(X_train)
+y_test_pred = tree.predict(X_test)
+tree_train = accuracy_score(y_train, y_train_pred)
+tree_test = accuracy_score(y_test, y_test_pred)
+print('Decision tree train/test accuracies %.3f/%.3f' %
+      (tree_train, tree_test))
+
+bag = bag.fit(X_train, y_train)
+y_train_pred = bag.predict(X_train)
+y_test_pred = bag.predict(X_test)
+bag_train = accuracy_score(y_train, y_train_pred)
+bag_test = accuracy_score(y_test, y_test_pred)
+print('Bagging train/test accuracies %.3f/%.3f' % (bag_train, bag_test))
+
+x_min = X_train[1, 0].min()-1
+x_max = X_train[:, 0].max()+1
+y_min = X_train[:, 1].min()-1
+y_max = X_train[:, 1].max()+1
+
+xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1),
+                     np.arange(y_min, y_max, 0.1))
+
+f, axarr = plt.subplots(nrows=1, ncols=2,
+                        sharex='col',
+                        sharey='row',
+                        figsize=(8, 3))
+for idx, clf, tt in zip([0, 1], [tree, bag], ['Decision tree', 'Bagging']):
+    clf.fit(X_train, y_train)
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    axarr[idx].contourf(xx, yy, Z, alpha=0.3)
+    axarr[idx].scatter(X_train[y_train == 0, 0],
+                       X_train[y_train == 0, 1], c='blue', marker='^')
+    axarr[idx].scatter(X_train[y_train == 1, 0],
+                       X_train[y_train == 1, 1], c='green', marker='o')
+    axarr[idx].set_title(tt)
+axarr[0].set_ylabel('Alcohol', fontsize=12)
+plt.text(0, -0.2, s='OD280/OD315 of diluted wines',
+         ha='center', va='center', fontsize=12, transform=axarr[1].transAxes)
+plt.show()
